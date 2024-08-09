ef155af511892b329d9623a71fb892b43144cc5e
Kang IWon <esfne4727@naver.com>
2024-08-09 22:31:43 +0900

    test

---
+++ src/test/java/com/sparta/spring_schedule/SpringScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
